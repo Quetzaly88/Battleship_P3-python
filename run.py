f9430869ca7b5ee7733ac19bcd862049e8558b39
Andrea Nilsson <andreaycoral@icloud.com>
2024-08-06 07:15:59 +0000

    Add dictionary with tuples and a representation of empty spot

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 import random
 
+class TableGame:
+    def __init__(self):
+        """
+        Start the board with a dictionary with tuples (x,y). 
+        use cero as empty spot. 
+        """
+        self.grid = {(x, y): "0" for x in range(5) for y in range(5)} #grid attribute represents dictionary
