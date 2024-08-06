ba0ab350c26b124971b193871340eee640133891
Andrea Nilsson <andreaycoral@icloud.com>
2024-08-06 07:26:38 +0000

    Create boards for two players

--- run.py
+++ run.py
@@ -4,6 +4,16 @@ class TableGame:
     def __init__(self):
         """
         Start the board with a dictionary with tuples (x,y). 
-        use cero as empty spot. 
+        X are numbers from 0-4 and y are letters A-E. Use * as empty spot. 
         """
-        self.grid = {(x, y): "0" for x in range(5) for y in range(5)} #grid attribute represents dictionary
+        self.grid = {(x, y): "*" for x in range(5) for y in 'ABCDE'}
+        # empty list
+        self.ships = []
+
+    
+def main():
+    #create board for two users. A person and the computer. 
+    tom_board = TableGame()
+    computer_board = TableGame()
+
+    
