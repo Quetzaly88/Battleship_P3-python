678bdb12e5c065c11dcf9615c4fd49f19dc00be4
Andrea Nilsson <andreaycoral@icloud.com>
2024-08-06 07:58:10 +0000

    Mark position on board with 'S'

--- run.py
+++ run.py
@@ -9,8 +9,18 @@ class TableGame:
         self.grid = {(x, y): "*" for x in range(5) for y in 'ABCDE'}
         # empty list
         self.ships = []
-
     
+
+    def place_ship(self, x, y):
+        """
+        Place a ship on the board and mark position with S, 
+        representing the Ship on the board.
+        """
+        self.grid[(x, y)] = 'S'
+        #add the position and append it to list
+        self.ships.append((x, y))
+
+        
 def main():
     #create board for two users. A person and the computer. 
     tom_board = TableGame()
